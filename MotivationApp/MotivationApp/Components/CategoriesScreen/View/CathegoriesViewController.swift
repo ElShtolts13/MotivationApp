f1ad38d06f1b77d52764b53c9b2ffb34d083f9df
Nadezhda <el.shtolts@yandex.ru>
2025-02-24 17:29:12 +0300

    .

---
+++ MotivationApp/MotivationApp/Components/CategoriesScreen/View/CathegoriesViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CathegoriesViewController.swift
+//  MotivationApp
+//
+//  Created by Надежда Капацина on 24.02.2025.
+//
+
+import UIKit
+
+class CathegoriesViewController: UIViewController {
+    
+    
+}
